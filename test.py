35f58fb092bd9d86595c870373214b06460aa847
Benitmulindwa <mulindwabenit@gmail.com>
2024-12-22 19:36:30 +0100

    update the test file

--- test.py
+++ test.py
@@ -1,49 +1,34 @@
-import json
-from flet import Page, Container, Text, Row, Column
+import flet as ft
 
 
-def process_figma_node(node, parent):
-    node_type = node.get("type")
-    children = node.get("children", [])
-    widget = None
-
-    if node_type == "FRAME":
-        widget = Column() if node.get("layoutMode") == "VERTICAL" else Row()
-        widget.controls.extend(
-            [process_figma_node(child, widget) for child in children]
+def main(page: ft.Page):
+    page.add(
+        ft.Stack(
+            [
+                ft.Container(
+                    left=5,
+                    top=5,
+                    width=150,
+                    height=50,
+                    bgcolor="#FF0000",
+                ),
+                ft.Container(
+                    left=40,
+                    top=55,
+                    width=100,
+                    height=100,
+                    bgcolor="#00FF00",
+                ),
+                ft.Container(
+                    content=ft.Text(value="FletEditor", size=24.0),
+                    left=9.0,
+                    top=367.0,
+                    width=125,
+                    height=29,
+                ),
+            ]
         )
-
-    elif node_type == "RECTANGLE":
-        fills = node.get("fills", [])
-        color = fills[0]["color"] if fills else {"r": 1, "g": 1, "b": 1}
-        widget = Container(
-            bgcolor=f"rgb({int(color['r']*255)}, {int(color['g']*255)}, {int(color['b']*255)})",
-            width=node["absoluteBoundingBox"]["width"],
-            height=node["absoluteBoundingBox"]["height"],
-        )
-
-    elif node_type == "TEXT":
-        style = node.get("style", {})
-        widget = Text(
-            value=node["characters"],
-            size=style.get("fontSize", 14),
-            weight=style.get("fontWeight", 400),
-        )
-
-    if widget and parent:
-        parent.controls.append(widget)
-
-    return widget
+    )
 
 
-def generate_flet_app(figma_data):
-    figma_structure = json.loads(figma_data)
-    root = figma_structure["document"]["children"][0]  # Assuming single page
-    page = Page()
-    process_figma_node(root, page)
-    return page
-
-
-test_data = """"""
-
-print(generate_flet_app(test_data))
+ft.app(target=main)
